29068f76ac14d6e0971343b39d5231a5ebed9195
Niraj <amatyaraj@gmail.com>
2019-11-12 12:16:19 +1100

    testing with user input-not able to match random word with user input

--- index.rb
+++ index.rb
@@ -1,3 +1,26 @@
 array_words = ["apple", "Library", "Book", "Boat", "Boy", "Girl", "bag"]
 
-p generate_random_word = array_words.sample.chars.shuffle.join("        ")
+ generate_random_word = array_words.sample.chars.shuffle.join("        ")
+
+ puts "Please input your username?"
+
+ username = gets.chomp
+
+ puts " Welcome to the words guessing game #{username}"
+ puts "Here is your first word"
+
+ p generate_random_word
+
+ puts "Please guess the right word."
+
+ player_guess_word = gets.chomp
+
+ if player_guess_word == generate_random_word
+    puts "you won the game"
+
+#  elsif 
+#     puts "Would you like to try again?"
+#         response = gets.chomp
+ end
+
+        
