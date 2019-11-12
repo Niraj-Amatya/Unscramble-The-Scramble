71b3dcce11909a5ecd64778d1a0a10b7a771d0e7
Niraj <amatyaraj@gmail.com>
2019-11-12 12:34:48 +1100

    new variable 'new_meaningless_word' added

--- index.rb
+++ index.rb
@@ -1,26 +1,36 @@
 array_words = ["apple", "Library", "Book", "Boat", "Boy", "Girl", "bag"]
 
- generate_random_word = array_words.sample.chars.shuffle.join("        ")
+generate_random_word = array_words.sample
+p new_meaningless_word =generate_random_word.chars.shuffle.join("        ")
 
- puts "Please input your username?"
+# user_input = 'yes'
+# puts "Do you want to play Words Guessing Game?"
+# puts "Please enter 'yes' to play and 'no' to end"
 
- username = gets.chomp
+# user_input = gets.chomp
 
- puts " Welcome to the words guessing game #{username}"
- puts "Here is your first word"
+# while user_input == "yes"
 
- p generate_random_word
+#     puts "Please input your username?"
 
- puts "Please guess the right word."
+#     username = gets.chomp
 
- player_guess_word = gets.chomp
+#     puts " Welcome to the words guessing game #{username}"
+#     puts "Here is your first word"
 
- if player_guess_word == generate_random_word
-    puts "you won the game"
+#     p generate_random_word
 
-#  elsif 
-#     puts "Would you like to try again?"
-#         response = gets.chomp
- end
+#     puts "Please guess the right word."
+
+#     player_guess_word = gets.chomp
+
+#     if 
+#         player_guess_word == array_words
+        
+#     puts "you won the game"
+
+# #   
+#     end
+# end
 
         
