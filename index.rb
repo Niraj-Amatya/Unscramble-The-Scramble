d33fe7a9db8c0c52b6c6b79a6a85894adcd662e4
Niraj <amatyaraj@gmail.com>
2019-11-12 11:17:43 +1100

    meaning less word generated using sample,chars, shuffle and join

--- index.rb
+++ index.rb
@@ -1,3 +1,3 @@
-array_words = ["apple", "Library", "Book", "Boat", "Boy", "Girl"]
+array_words = ["apple", "Library", "Book", "Boat", "Boy", "Girl", "bag"]
 
-p random_word = array_words.sample
+p generate_random_word = array_words.sample.chars.shuffle.join("        ")
