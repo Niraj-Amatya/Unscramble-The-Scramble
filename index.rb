67d856a29ce78b7449a86e35c579f89242af0f7b
Niraj <amatyaraj@gmail.com>
2019-11-13 21:28:22 +1100

    method created to check user input conditions

--- index.rb
+++ index.rb
@@ -1,36 +1,75 @@
-array_words = ["apple", "Library", "Book", "Boat", "Boy", "Girl", "bag"]
 
-generate_random_word = array_words.sample
-p new_meaningless_word =generate_random_word.chars.shuffle.join("        ")
+def main()
+     user_input = 'yes'
+        puts "Do you want to play Words Guessing Game?"
+        puts "Please enter 'yes' to play and 'no' to end"
 
-# user_input = 'yes'
-# puts "Do you want to play Words Guessing Game?"
-# puts "Please enter 'yes' to play and 'no' to end"
+        user_input = gets.chomp
 
-# user_input = gets.chomp
+        puts "Please input your username?"
 
-# while user_input == "yes"
+        username = gets.chomp
 
-#     puts "Please input your username?"
+        puts " Welcome to the words guessing game #{username}"
+        puts "Here is your first word"
 
-#     username = gets.chomp
 
-#     puts " Welcome to the words guessing game #{username}"
-#     puts "Here is your first word"
 
-#     p generate_random_word
+        while user_input == "yes"
 
-#     puts "Please guess the right word."
+        array_words = ["apple", "Library", "Book", "Boat", "Boy", "Girl", "bag"]
 
-#     player_guess_word = gets.chomp
-
-#     if 
-#         player_guess_word == array_words
-        
-#     puts "you won the game"
-
-# #   
-#     end
-# end
+        generate_random_word = array_words.sample
+        generate_new_meaningless_word =generate_random_word.chars.shuffle.join("        ")
 
         
+
+        p generate_new_meaningless_word
+
+        match_the_word(generate_random_word)
+
+        puts "Do you want to play again?"
+        user_input = gets.chomp
+        end
+    end
+
+    
+    
+    def match_the_word(generate_random_word)
+
+        
+        
+        puts "Please guess the right word."
+        player_guess_word = gets.chomp
+
+        # if player_guess_word == "pass"
+        #     p generate_new_meaningless_word
+        #     player_guess_word = gets.chomp
+
+
+        if
+            player_guess_word != generate_random_word
+            puts "you loose" 
+        
+        elsif 
+            player_guess_word == generate_random_word
+            puts "you win" 
+            
+            else
+            puts "Do something"
+
+    # #   
+        end
+    
+    end
+
+    
+
+
+
+# player_guess_word == 'pass'
+#         puts "Here is your new word."
+#         p generate_new_meaningless_word
+#         player_guess_word = gets.chomp   
+
+main()
