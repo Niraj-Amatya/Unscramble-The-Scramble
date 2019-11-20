8212c874abe55fccc0dbbe149fdd361b69a141b9
Niraj <amatyaraj@gmail.com>
2019-11-20 12:04:21 +1100

    code refactoring

--- index.rb
+++ index.rb
@@ -8,28 +8,30 @@ app_name = Artii::Base.new
 puts app_name.asciify("Unscramble The Scramble").colorize(:blue).bold
 
 # puts "Please enter your name player_1:".colorize(:red)
-player_1 = cmd_args[0].upcase
+player_1 = cmd_args[0].upcase.colorize(:blue)
 
 # puts "Please enter your name player_2:".colorize(:red)
-player_2 = cmd_args[1].upcase
+player_2 = cmd_args[1].upcase.colorize(:yellow)
 # gets.chomp.upcase.colorize(:blue).bold
 ARGV.clear
 
 
 def greeting(person1, person2)
-    puts "========================================================================="
+    sleep(1)
+    127.times{print "="}
+    puts
     puts "Welcome to the world of Guessing game #{person1} and #{person2}."
     puts "A bit of brief about the Game:"
     puts "There are two players in this game: Player_1 and Player_2."
     puts "Player_1 is the provider of words and Player_2 will be the guesser."
     puts "#{person1} you are the provider and #{person2} you are the guesser."
     puts "Best of Luck!!!!!"
-    puts "=========================================================================="
 
+    127.times{print "="}
     puts
     puts
 
-
+        sleep(2)
         puts "You are the Player_1 #{person1}."
         puts "Remember the word can only be strings"
         puts "Just not to confuse, you can't see anything you are typing in the screen. So hit enter to enter the word."
@@ -52,30 +54,37 @@ def run_game(first, second)
         
         array_words = []
         puts
+        sleep(1.5)
         print "Please input your word #{first_player}:"
-
-
         user_input = STDIN.noecho(&:gets).chomp.upcase
 
         
             # puts "Input the correct string"
         if /^[a-zA-Z]+$/.match(user_input)
+            puts
+            puts
             array_words << user_input
         
             generate_random_word = array_words.sample
-            puts
-            puts
-            puts
-            puts "You are the Player_2, #{second_player}."
-            puts "Please guess your word wisely."
-            puts
-            puts "Here is your word to Guess #{second_player}"
-            puts
-    
-            #generate new meaningless word
+            sleep(1)
             generate_new_meaningless_word = generate_random_word.chars.shuffle.join(" ")
             app_name = Artii::Base.new 
+            puts "Here is the word to guess:"
+            puts
             puts app_name.asciify(generate_new_meaningless_word).colorize(:green)
+            
+            
+            puts
+            sleep(1)
+            puts "You are Player_2, #{second_player}."
+            # puts "You are the Guesser."
+            
+            # sleep(2.5)
+            # puts "Here is your word to Guess #{second_player}"
+            # puts
+            # sleep(1.5)
+            #generate new meaningless word
+            
 
             user_guess = " "
             guess_count = 0
@@ -85,11 +94,21 @@ def run_game(first, second)
             while user_guess != generate_random_word && !out_of_guesses
                 if guess_limit > guess_count
                     
+                    # puts
+                    # puts
+                    # sleep(2)
+                    
+                    # puts
+                    
+                    sleep(1)
+                    
                     puts
-                    puts "Reminder! you have #{guess_limit} chance to Unscramble this Scrambled word #{second}."
-                    puts
-                    guess_limit -= 1
+                    puts "Reminder! you have #{guess_limit} chance to Unscramble this Scrambled word.".colorize(:red)
+                    puts "Best of Luck!!!"
                     print "Please guess the right word #{second_player}: "
+                    
+                    
+                    guess_limit -= 1
                     user_guess = gets.chomp.upcase
                     
 
@@ -101,24 +120,27 @@ def run_game(first, second)
             end
                     if out_of_guesses == true
                         puts
-                        puts "Oh! you have guessed it wrong #{second}."
                         puts
-                        puts "The right word is: #{generate_random_word}"
+                        puts "Oops! Wrong guess #{second}.".colorize(:red)
+                        puts
+                        puts
+                        sleep(1)
+                        puts "The right word is:" + "#{generate_random_word}".colorize(:green)
     
                     elsif out_of_guesses == false
                         puts
-                        puts "Right guess #{second}"
+                        puts
+                        sleep(1)
+                        puts "Right guess #{second}.Good job!!!".colorize(:green)
                          score += 1
-                         
-                
                     end
 
             
-            else puts
-            puts
-            puts "Invalid input."
-            puts "Please input only strings"
-            puts
+                else puts
+                puts
+                puts "Invalid input."
+                puts "Please input only strings."
+                puts
             
         
             
@@ -130,36 +152,53 @@ def run_game(first, second)
     # puts "#{second} your total score is  #{score}"
 end
 
-begin
 
-        score_player2 = run_game(player_1, player_2)
 
-        # puts "You have scored #{score_player2} #{player_2}"
+    begin
+            score_player2 = run_game(player_1, player_2)
+            puts
+            puts
+            sleep(2)
+            puts "Now lets switch the game between the players."
+            puts
+            puts
+            score_player1 = run_game(player_2, player_1)
+    
+
+    # rescue
+    #     puts "We did not anticipate things going worng. We will try to fix this as soon as possible."
+
+   
+            puts
+            puts
+            sleep(2)
+            puts "Your score for this game is" + " #{score_player1},".colorize(:green) + " #{player_1}."
+            puts "Your score for this game is" + " #{score_player2},".colorize(:green) + " #{player_2}."
+
+            if score_player1 > score_player2
+                puts
+                puts
+                sleep(2)
+                puts "Congratulation #{player_1}, you are the winner."
+                # puts "Your score is #{score_player1} and #{player_2} score is #{score_player2}"
+
+            elsif score_player2 > score_player1
+                puts
+                puts
+                sleep(2)
+                puts "Congratulation #{player_2} you are the winner."
+                # puts "Your score is #{score_player2} and #{player_1} score is #{score_player1}"
+                
+
+            else
+                puts "Congratulation, you both are the winner."
+
+            end
+    rescue
+        puts "We did not anticipate things going worng. We will try to fix it as soon as possible."
         puts
-        puts "Now its your turn to input the word #{player_2} and you are the guesser #{player_1}"
-        score_player1 = run_game(player_2, player_1)
+        puts
 
-rescue
-    puts "We did not anticipate things going worng. We will try to fix this as soon as possible."
+    end
 
-end
-
-        # puts "You have scored #{score_player1} #{player_1}"
-        puts "Player 1 score = #{score_player1}"
-        puts "Player 2 score = #{score_player2}"
-
-
-
-        if score_player1 > score_player2
-            puts "#{player_1} is the winner"
-            # puts "Your score is #{score_player1} and #{player_2} score is #{score_player2}"
-
-        elsif score_player2 > score_player1
-            puts "#{player_2} is the winner"
-            # puts "Your score is #{score_player2} and #{player_1} score is #{score_player1}"
-            
-
-        else
-            puts "You both are the winner"
-
-        end
+    
